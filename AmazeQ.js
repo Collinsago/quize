1bc4e959d3f43ca4ac0f1e15022847f0540ee709
Collins Agofure <71197307+Collinsago@users.noreply.github.com>
2022-06-17 01:37:07 +0100

    Add files via upload

---
+++ AmazeQ.js
@@ -0,0 +1,723 @@
+
+//creating an array and passing the number, questions, options amd answers
+let MCQS = [
+
+    {
+        question: "In questions below, choose the word(s) or phrase which best fills the gap(s) <br><br> Jubril found that thieves had entered his house in his absence. He went to the police to report the ........ ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+
+    {
+        question: "After the accused was found guilty by the court, his counsel ...... before sentence was passed ",
+        answer:"made a plea for mitigation",
+        options:[
+         "begged for mercy ",
+        "made a plea for mitigation ",
+         "made an ovation",
+        "made a plea for litigation",
+        ]        
+    },
+
+
+    {
+        question: "Four persons accused of currency ........ are being held by the security agents ",
+        answer:"trafficking",
+        options:[
+         "tracfficking ",
+        "traviking ",
+         "trafficking",
+        "traffiking",
+        ]        
+    },
+
+
+    {
+        question: "Gone are the days when he .......... enjoy patronage.",
+        answer:"used to",
+        options:[
+         "would ",
+        "will",
+         "used to",
+        "could",
+        ]        
+    },
+
+
+    {
+        question: "He did not attend the final burial ........... ",
+        answer:"rites",
+        options:[
+         "rite ",
+        "rights ",
+         "rites",
+        "right",
+        ]        
+    },
+
+
+    {
+        question: "The officer was compelled to .......... the suspect's car.  ",
+        answer:"seize",
+        options:[
+         "seize ",
+        "cease ",
+         "sieze",
+        "sease",
+        ]        
+    },
+
+
+    {
+        question: "A number of doctors are not so well disposed to ......... in government hospitals these days, ....... they? ",
+        answer:"working/are",
+        options:[
+         "working/do ",
+        "work/are ",
+         "working/are ",
+        "work/aren't",
+        ]        
+    },
+
+
+    {
+        question: "If you are going to the market, may I ........ please? ",
+        answer:"come with you",
+        options:[
+         "follow you ",
+        "come with you ",
+         "come by you",
+        "come as your second",
+        ]        
+    },
+
+
+    {
+        question: "Wada said that two of his chickens ......... eggs yesterday morning ",
+        answer:"layed",
+        options:[
+         "layed ",
+        "lied ",
+         "laid",
+        "lain",
+        ]        
+    },
+
+
+    {
+        question: "Thank you for the party, we really ........ ",
+        answer:"enjoyed",
+        options:[
+         "enjoyed ourselves  ",
+        "enjoyed ",
+         "enjoyed very much",
+        "enjoyed too much.",
+        ]        
+    },
+    {
+        question: "In questions below, choose the option nearest in meaning to the word(s) or phrase underlined <br><br> The culprits will  <u> surrender </u>  their loot to the customs officials. ",
+        answer:"give in",
+        options:[
+         "give away ",
+        "give out  ",
+         "give in",
+        "give up",
+        ]        
+    },
+    {
+        question: "The discussion became  <u> animated </u>.  ",
+        answer:"lively",
+        options:[
+         "specialized ",
+        "lively ",
+         "intellectual",
+        "unruly",
+        ]        
+    },
+    {
+        question: "Rich citizens are often  <u> niggardly </u> in their ways ",
+        answer:"miserly",
+        options:[
+         "beggarly ",
+        "sordid ",
+         "miserly",
+        "pompous ",
+        ]        
+    },
+    {
+        question: "Olu promised to  <u> look out for </u> her next time he is in town ",
+        answer:"visit",
+        options:[
+         "watch for ",
+        "visit ",
+         "greet",
+        "take care of",
+        ]        
+    },
+    {
+        question: "Finally, I assured him that I would not <u> go back on my word </u>. ",
+        answer:"fail to keep my promise",
+        options:[
+         "return to a place ",
+        "return to a certain word when reading ",
+         "re-use my word",
+        "fail to keep my promise",
+        ]        
+    },
+    {
+        question: "The speaker <u> took his audience down memory lane </u> before he went to the topic of his lecture.  ",
+        answer:"recalled events from the past",
+        options:[
+         "took his audience on a journey through a street called Memory Lane ",
+        "recalled events from the past ",
+         "told a tale about a place called Memory Lane",
+        "gave an outline of his lecture",
+        ]        
+    },
+    {
+        question: "Choose the option that has the same vowel sound as the one represented by the letters underlined <br><br> f<u>ae</u>ces ",
+        answer:"peasant ",
+        options:[
+         "polices ",
+        "pain ",
+         "peasant ",
+        "pear",
+        ]        
+    },
+    {
+        question: "p<u>o</u>t  ",
+        answer:"what",
+        options:[
+         "port ",
+        "word ",
+         "correct",
+        "what",
+        ]        
+    },
+    {
+        question: "l<u>eo</u>pard",
+        answer:"led",
+        options:[
+         "geology ",
+        "people",
+         "leaven",
+        "led",
+        ]        
+    },
+    {
+        question: "Select the word that has the same pattern of stress as the given word <br><br> EDUCATION ",
+        answer:"economics",
+        options:[
+         "duplicity ",
+        "conjectural",
+         "economics",
+        "elaborate",
+        ]        
+    },
+    {
+        question: "HONOUR ",
+        answer:"hotel",
+        options:[
+         "hyena ",
+        "human ",
+         "humane",
+        "hotel",
+        ]        
+    },
+    {
+        question: "Identify the word that has a different stress pattern from the others. ",
+        answer:"discourse",
+        options:[
+         "discourse ",
+        "intimate  ",
+         "accelerate",
+        "integral",
+        ]        
+    },
+    {
+        question: "Identify the word that has the stress on the second syllable.  ",
+        answer:"result ",
+        options:[
+         "always ",
+        "result  ",
+         "reproduce",
+        "understand",
+        ]        
+    },
+    {
+        question: "Choose the option opposite in meaning to the word(s) or phrase in italics. <br><br> Common sense requires that one should be <em> decorous </em> at formal gatherings.  ",
+        answer:"impolite",
+        options:[
+         "courteous",
+        "careful",
+         "impolite",
+        "incurious",
+        ]        
+    },
+    {
+        question: "The string was <em> taut </em> ",
+        answer:"loose",
+        options:[
+         "stretched",
+        "loose",
+         "firm",
+        "tight",
+        ]        
+    },
+    {
+        question: "The diminutive figure bounces over the track with <em> unfathomable </em> lightness ",
+        answer:"predictable ",
+        options:[
+         "irresistible",
+        "predictable ",
+         "invigorating",
+        "impressive",
+        ]        
+    },
+    {
+        question: "The teacher announced that the class would be making a <em> fleeting </em> visit to the Zoo. ",
+        answer:"long",
+        options:[
+         "routine",
+        "long ",
+         "brief",
+        "guided",
+        ]        
+    },
+    {
+        question: "We intend to make the reception a <em> diurnal </em> event.",
+        answer:"a nightly",
+        options:[
+         "an annual",
+        "a weekly",
+         "a nightly",
+        "a short.",
+        ]        
+    },
+    {
+        question: "The project is designed to <em>alleviate</em> poverty in the country. ",
+        answer:"exacerbate",
+        options:[
+         "tackle ",
+        "eradicate",
+         "assuage",
+        "exacerbate",
+        ]        
+    },
+    {
+        question: "The permanent Secretary left his job <em>under a cloud</em> ",
+        answer:"with a clear record",
+        options:[
+         "on a sunny day ",
+        "with a clear record ",
+         "in disgrace",
+        "on a cloudy clay.",
+        ]        
+    },
+    {
+        question: "It is surprising that this politician has now turned <em>renegade</em> ",
+        answer:"loyal ",
+        options:[
+         "militant ",
+        "quiet ",
+         "loyal ",
+        "corrupt",
+        ]        
+    },
+    {
+        question: "Hypertension is a <em>debilitating</em> disease that everyone should prevent.",
+        answer:"an energizing",
+        options:[
+         "a horrible  ",
+        "an enervating",
+         "a mortal",
+        "an energizing",
+        ]        
+    },
+    {
+        question: "The striking workers have vowed not to return to work until the decision is <em>rescinded</em> ",
+        answer:"implemented ",
+        options:[
+         "changed",
+        "incorporated",
+         "implemented ",
+        "dismissed",
+        ]        
+    },
+    {
+        question: "It is rare for a leader and his deputy to <em>see eye to eye</em>. ",
+        answer:"be of one mind ",
+        options:[
+         "reproach each other ",
+        "disagree ",
+         "be of one mind ",
+        "respect each other.",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+    {
+        question: "In questions b to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+   
+];
